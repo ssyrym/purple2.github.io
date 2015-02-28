006396ed893205850d5682e73ded70f1afd2abc5
ssyrym <ssyrym@users.noreply.github.com>
2015-02-27 21:01:38 -0800

    Create bar.js
    Bar object, shows the fighter health. Image might change. This image for testing

---
+++ bar.js
@@ -0,0 +1,37 @@
+function Bar() {
+    this.LEFTbar = ASSET_MANAGER.getAsset("./img/lifebarLEFT.png");
+    this.RIGHTbar = ASSET_MANAGER.getAsset("./img/lifebarRIGHT.png");
+    this.greenSprite = ASSET_MANAGER.getAsset("./img/green.png");
+
+    this.barwidth = 220;
+    this.barheight = 30;
+
+    this.greenheight = 16;
+    this.greenLEFTwidth = 210;
+    this.greenRIGHTwidth = 210 - 50;
+
+    this.x1 = 50;
+    this.x2 = 1050;
+
+    this.y = 100;
+
+    this.totalHits = 0;
+}
+
+Bar.prototype.drawBar = function (ctx) {
+    ctx.drawImage(this.LEFTbar, 0, 0, this.barwidth, this.barheight, this.x1, this.y, this.barwidth, this.barheight);
+    ctx.drawImage(this.greenSprite, 0, 0, this.greenLEFTwidth, this.greenheight, this.x1 + 4, this.y + 7, this.greenLEFTwidth, this.greenheight);
+
+    ctx.drawImage(this.RIGHTbar, 0, 0, this.barwidth, this.barheight, this.x2, this.y, this.barwidth, this.barheight);
+    ctx.drawImage(this.greenSprite, 0, 0, this.greenRIGHTwidth, this.greenheight, this.x2 + 6, this.y + 7, this.greenRIGHTwidth, this.greenheight);
+}
+
+
+
+Bar.prototype.decreaseHealth = function (isLeftFihter) {
+    if(isLeft){
+        this.greenLEFTwidth = this.greenLEFTwidth - 21;
+    } else {
+        this.greenRIGHTwidth = this.greenRIGHTwidth - 21;
+    }
+}
