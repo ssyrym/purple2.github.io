5eaed157e45a216b09d8965caa58ffb8d6fb0311
purple2 <css491purple2@gmail.com>
2015-02-28 15:11:27 -0800

    Update main.js
    Updated the image name 
    Syrym

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ ASSET_MANAGER.queueDownload("./img/staircase.png");
 
 ASSET_MANAGER.queueDownload("./img/lifebarRIGHT.png");
 ASSET_MANAGER.queueDownload("./img/lifebarLEFT.png");
-ASSET_MANAGER.queueDownload("./img/green.png");
+ASSET_MANAGER.queueDownload("./img/green1.png");
 
 ASSET_MANAGER.downloadAll(function () {
     console.log("starting up da sheild");
