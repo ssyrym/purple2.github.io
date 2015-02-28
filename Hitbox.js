fa87cdb9d32085fe2b27c1c8597245b49a861819
purple2 <css491purple2@gmail.com>
2015-02-28 04:16:07 -0800

    Update Hitbox.js

--- Hitbox.js
+++ Hitbox.js
@@ -1,8 +1,8 @@
 function Hitbox(game, id) {
     this.game = game;
     this.ID = id;
-    this.hitbox = { x: 0, y: 0, width: 0, height: 0 }
-    this.attackbox = { x: 0, y: 0, width: 0, height: 0 }
+    this.hitbox = { x: 0, y: 0, width: 0, height: 0 };
+    this.attackbox = { x: 0, y: 0, width: 0, height: 0 };
     this.attack = false;
 }
 
@@ -42,33 +42,55 @@ Hitbox.prototype.collideRight = function () {
 Hitbox.prototype.attackenemy = function () {
     var ent = this.game.entities[1];
     var ent2 = this.game.entities[2];
+    var damage = 1;
     if (ent.myboxes.ID != this.ID) {
         console.log(this.attack);
         console.log("ent2 ID = this.ID check first ent");
+        
         if ((this.attack && !ent.gotHit &&
             (this.attackbox.x < ent.myboxes.hitbox.x + ent.myboxes.hitbox.width)) &&
             (this.attackbox.x + this.attackbox.width > ent.myboxes.hitbox.x) &&
             (this.attackbox.y < ent.myboxes.hitbox.y + ent.myboxes.hitbox.height) &&
             (this.attackbox.height + this.attackbox.y > ent.myboxes.hitbox.y)) {
-            console.log("Player hit");// this means that ent is hit which is a player entity and can be altered here
-        }
             
-    }// 
-    else if (ent2.myboxes.ID != this.ID) {
+            if (ent.blockLeft || ent.blockRight) {
+                damage = .5;
+            }
+            if (ent2.wkPunch || ent2.wkKick) {
+                damage *= 4;
+            } else {
+                damage *= 8;
+            }
+            
+            ent.bar.decreaseHealth( damage );
+            console.log("Player hit lost " + damage + "health");
+        }
+    } else if (ent2.myboxes.ID != this.ID) {
         console.log(this.attackbox.x < ent2.myboxes.hitbox.x + ent2.myboxes.hitbox.width);
         console.log("ent.ID = this.ID check for second");
+
         if ((this.attack && !ent2.gotHit &&
             (this.attackbox.x < ent2.myboxes.hitbox.x + ent2.myboxes.hitbox.width)) &&
             (this.attackbox.x + this.attackbox.width > ent2.myboxes.hitbox.x) &&
             (this.attackbox.y < ent2.myboxes.hitbox.y + ent2.myboxes.hitbox.height) &&
             (this.attackbox.height + this.attackbox.y > ent2.myboxes.hitbox.y)) {
-            console.log("Enemy Player hit");
 
-                }
+            if (ent.blockLeft || ent.blockRight) {
+                damage = .5;
             }
-        //}
-    //}
+            if (ent2.wkPunch || ent2.wkKick) {
+                damage *= 4;
+            } else {
+                damage *= 8;
+            }
+
+            console.log("Player hit lost " + damage + "health");
+            ent2.bar.decreaseHealth(damage);
+            
+        }
+    }
 }
+
 
 Hitbox.prototype.attackPlayer = function () {
     for (var i = 0; i < this.game.entities.length; i++) {
