00d4820dcc8335c203fd8d818627b9cb522d9b75
purple2 <css491purple2@gmail.com>
2015-02-28 15:49:58 -0800

    Update bar.js
    The new bar code.
    Syrym

--- bar.js
+++ bar.js
@@ -3,20 +3,26 @@ function Bar(game, the_fighter) {
     this.game = game;
     this.bar = this.fighter.isPlayer ? ASSET_MANAGER.getAsset("./img/lifebarLEFT.png") : ASSET_MANAGER.getAsset("./img/lifebarRIGHT.png");
 
-    this.greenSprite = ASSET_MANAGER.getAsset("./img/green.png");
+    this.greenRedSprite = ASSET_MANAGER.getAsset("./img/green1.png");
 
-    this.barwidth = 220;
-    this.barheight = 30;
 
-    this.greenheight = 16;
-    this.greenwidth = 210;
-    this.greenX = this.fighter.isPlayer ? 54 : 1056;
-    //this.greenRIGHTwidth = 210;
+    this.barwidth = 400;
+    this.barheight = 180;
 
-    this.x = this.fighter.isPlayer ? 50 : 1050;
+    this.greenRedheight = 5;
+    this.redwidth = 256;
+    this.greenwidth = 256;
+
+    this.x = this.fighter.isPlayer ? 70 : 900;
     //this.x2 = 1050;
 
-    this.y = 100;
+    this.y = 5;
+
+    this.greenX = this.fighter.isPlayer ? this.x + 88 : this.x + 55;
+    this.greenRedY = this.y + 63
+    this.redX = this.fighter.isPlayer ? 70 + 88 : 900 + 55;
+    //this.greenRIGHTwidth = 210;
+
     //this.hitDamage = 21;
     this.totalHits = 0;
 
@@ -32,16 +38,15 @@ Bar.prototype.constructor = Bar;
 Bar.prototype.update = function () { }
 
 Bar.prototype.draw = function (ctx) {
-    
-    ctx.drawImage(this.bar, 0, 0, this.barwidth, this.barheight, this.x, this.y, this.barwidth, this.barheight);
-    ctx.drawImage(this.greenSprite, 0, 0, this.greenwidth, this.greenheight, this.greenX, this.y + 7, this.greenwidth, this.greenheight);
-    
+    ctx.drawImage(this.greenRedSprite, 0, 17, this.redwidth, this.greenRedheight, this.redX, this.greenRedY, this.redwidth, this.greenRedheight); //red bar 
+    ctx.drawImage(this.greenRedSprite, 0, 0, this.greenwidth, this.greenRedheight, this.greenX, this.greenRedY, this.greenwidth, this.greenRedheight); // green bar
+    ctx.drawImage(this.bar, 0, 0, this.barwidth, this.barheight, this.x, this.y, this.barwidth, this.barheight); // health bar
+
     Entity.prototype.draw.call(this);
 }
 
-Bar.prototype.decreaseHealth = function ( damage ) {
+Bar.prototype.decreaseHealth = function (damage) {
     this.fighter.isPlayer ? this.greenwidth -= damage : this.greenX += damage, this.greenwidth -= damage;
 }
-
 
 
