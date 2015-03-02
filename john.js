488757f14d80d9cc00d13a54efffb25bd2984ed4
purple2 <css491purple2@gmail.com>
2015-03-01 20:53:03 -0800

    Update john.js

--- john.js
+++ john.js
@@ -378,14 +378,14 @@ John.prototype.update = function () {
 }
 
 John.prototype.draw = function (ctx) {
-    ctx.fillStyle = "DarkGreen";
+    /*ctx.fillStyle = "DarkGreen";
     ctx.fillRect(this.myboxes.hitbox.x, this.myboxes.hitbox.y, this.myboxes.hitbox.width, this.myboxes.hitbox.height);
     Entity.prototype.draw.call(this);
     if (this.current_action) {
         ctx.fillStyle = "Red";
         ctx.fillRect(this.myboxes.attackbox.x, this.myboxes.attackbox.y, this.myboxes.attackbox.width, this.myboxes.attackbox.height);
         Entity.prototype.draw.call(this);
-    }
+    }*/
 
     if (this.jumping) {
         if (this.isRight) {
